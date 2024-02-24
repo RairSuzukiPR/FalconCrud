1c8277c8f5c34b71bc10e80391fbaaf6a75eba67
rairsuzuki <rairsuzukipr@gmail.com>
2024-02-23 21:08:29 -0300

    classif api

--- app/resources/NeuralNetResource.py
+++ app/resources/NeuralNetResource.py
@@ -1,87 +1,29 @@
-import random
-
 import falcon
 import json
 import torch
-import numpy as np
-# from falcon.media.validators.jsonschema import validate
-# from app.schemas import load_schema
-from app.services.model import Linear_QNet, QTrainer
-from collections import deque
-
-MAX_MEMORY = 100_000
-BATCH_SIZE = 1000
+from app.services.model import NeuralNet, Trainer
 
 
 class ModelTrainerAgentManager:
     def __init__(self):
-        self.n_games = 0
-        self.wins = 0
-        # self.epsilon = 0  # randomness
-        self.gamma = 0  # discount rate
-        self.learning_rate = 0
-        self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
         self.neural_net = None
         self.trainer = None
 
-    def initialize_neural_net(self, input_size, hidden_size, output_size, learning_rate, gamma):
-        self.gamma = gamma
-        self.learning_rate = learning_rate
-        self.neural_net = Linear_QNet(input_size, hidden_size, output_size)
-        self.trainer = QTrainer(self.neural_net, lr=learning_rate, gamma=gamma)
+    def initialize_neural_net(self, input_size, hidden1_size, hidden2_size, output_size, learning_rate, epochs):
+        self.neural_net = NeuralNet(input_size, hidden1_size, hidden2_size, output_size)
+        self.trainer = Trainer(self.neural_net, lr=learning_rate, epochs=epochs)
 
     def predict(self, state):
-        # print(state)
         if self.neural_net and self.trainer:
-            self.neural_net.eval()
-            final_move = [0, 0, 0, 0, 0]
-            state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.neural_net(state0)
-            # print('-----start-----')
-            # print('prediction -> ', prediction)
-            move = torch.argmax(prediction).item()
-            # print('move -> ', move)
-            # print('return -> ', int(np.array(move).argmax()))
-            # print('-----end-----')
-            final_move[move] = 1
-            # return move
-            return int(np.array(final_move).argmax())
-        return 0  # TODO: temp
+            new_state = torch.FloatTensor(state)
+            with torch.no_grad():
+                print("best action ->", self.trainer.model(new_state).argmax().item())
+                return self.trainer.model(new_state).argmax().item()
+            return -1
 
-    def train(self, state_old, action, reward, next_state, done):
+    def train(self):
         if self.neural_net and self.trainer:
-            self.trainer.train_step(state_old, action, reward, next_state, done)
-            self._remember(state_old, action, reward, next_state, done)
-
-            if done:
-                self.n_games += 1
-                self._train_long_memory()
-                self.neural_net.save()
-
-                # TODO: entender
-                # if score > record:
-                #     record = score
-                #     self.neural_net.save()
-
-                # TODO: ver uma forma de plotar wins por game que nao usou acao radom, usar reward por round
-                # plot_scores.append(score)
-                # total_score += score
-                # mean_score = total_score / agent.n_games
-                # plot_mean_scores.append(mean_score)
-                # plot(plot_scores, plot_mean_scores)
-    def _remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
-
-    def _train_long_memory(self):
-        if len(self.memory) > BATCH_SIZE:
-            mini_sample = random.sample(self.memory, BATCH_SIZE)  # list of tuples
-        else:
-            mini_sample = self.memory
-
-        states, actions, rewards, next_states, dones = zip(*mini_sample)
-        # print(states.shape)
-        self.trainer.train_step(states, actions, rewards, next_states, dones)
-
+            return self.trainer.train()
 
 
 manager = ModelTrainerAgentManager()
@@ -104,26 +46,19 @@ class NeuralNetResource:
         res = req.get_media()
         manager.initialize_neural_net(
             res.get('input_size'),
-            res.get('hidden_size'),
+            res.get('hidden1_size'),
+            res.get('hidden2_size'),
             res.get('output_size'),
             res.get('learning_rate'),
-            res.get('gamma')
+            res.get('epochs')
         )
 
         resp.status = falcon.HTTP_200
         resp.text = json.dumps({'message': 'Neural net and trainer initialized successfully'})
 
     def on_patch(self, req, resp):
-        res = req.get_media()
-        manager.train(
-            res.get('state'),
-            res.get('action'),
-            res.get('reward'),
-            res.get('new_state'),
-            res.get('done')
-        )
-
         resp.status = falcon.HTTP_200
+        resp.text = json.dumps({'message': manager.train()})
 
 
 NeuralNetResource = NeuralNetResource()
--- app/services/model.py
+++ app/services/model.py
@@ -1,74 +1,98 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
-import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-# estudar sk learn MLP!
-
-class Linear_QNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden1_size, hidden2_size, output_size):
         super().__init__()
-        self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, output_size)
+        self.fc1 = nn.Linear(input_size, hidden1_size)
+        self.fc2 = nn.Linear(hidden1_size, hidden2_size)
+        self.out = nn.Linear(hidden2_size, output_size)
 
     def forward(self, x):
-        x = F.relu(self.linear1(x))
-        # x = F.leaky_relu(self.linear1(x))  # permite um pequeno gradiente para valores negativos ajudando a mitigar o problema de "neurônios mortos"
-        x = self.linear2(x)
-        # print('forward -> ', x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.out(x)
+
         return x
 
-    def save(self, file_name='model.pth'):
-        model_folder_path = './model'
-        if not os.path.exists(model_folder_path):
-            os.makedirs(model_folder_path)
+    # def save(self, file_name='model.pth'):
+    #     model_folder_path = './model'
+    #     if not os.path.exists(model_folder_path):
+    #         os.makedirs(model_folder_path)
+    #
+    #     file_name = os.path.join(model_folder_path, file_name)
+    #     torch.save(self.state_dict(), file_name)
 
-        file_name = os.path.join(model_folder_path, file_name)
-        torch.save(self.state_dict(), file_name)
 
-
-class QTrainer:
-    def __init__(self, model, lr, gamma):
-        self.lr = lr
-        self.gamma = gamma
+class Trainer:
+    def __init__(self, model, lr, epochs):
         self.model = model
-        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.lr = lr
+        self.epochs = epochs
+        self.optimizer = torch.optim.Adam(model.parameters(), lr=self.lr)
         self.criterion = nn.CrossEntropyLoss()
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
 
-    def train_step(self, state, action, reward, next_state, done):
-        self.model.train()
-        self.optimizer.zero_grad()
+    def train(self):
+        torch.manual_seed(22)
 
-        state = torch.tensor(state, dtype=torch.float)
-        next_state = torch.tensor(next_state, dtype=torch.float)
-        action = torch.tensor(action, dtype=torch.long)
-        reward = torch.tensor(reward, dtype=torch.float)
-        # (n, x)
+        dados = pd.read_csv('training_data_tracker2.csv', sep=',', header=0)
+        X = dados.drop('comportamento', axis=1)
+        y = dados['comportamento']
 
-        if len(state.shape) == 1:
-            state = torch.unsqueeze(state, 0)
-            next_state = torch.unsqueeze(next_state, 0)
-            action = torch.unsqueeze(action, 0)
-            reward = torch.unsqueeze(reward, 0)
-            done = (done, )
+        X = X.values
+        y = y.values
 
-        print("---->", state.shape)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=22)
 
-        # 1: predicted Q values with current state
-        pred = self.model(state)
-        target = pred.clone()
-        for idx in range(len(done)):
-            Q_new = reward[idx]
-            if not done[idx]:
-                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+        # Convert X features to float tensors
+        self.X_train = torch.FloatTensor(self.X_train)
+        self.X_test = torch.FloatTensor(self.X_test)
 
-            target[idx][torch.argmax(action[idx]).item()] = Q_new
+        # Convert y labels to float tensors
+        self.y_train = torch.LongTensor(self.y_train)
+        self.y_test = torch.LongTensor(self.y_test)
 
-        # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
-        # verificar se o target sempre eh igual!
-        loss = self.criterion(pred, target)
-        # print(loss)
-        loss.backward()
+        # Train the model
+        losses = []
 
-        self.optimizer.step()
+        for i in range(self.epochs):
+            # Forward pass
+            y_pred = self.model.forward(self.X_train)
+
+            # Compute loss
+            loss = self.criterion(y_pred, self.y_train)
+
+            # Backpropagation
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            # Track losses
+            losses.append(loss.item())
+
+            if i % (self.epochs * 0.1) == 0:
+                print(f'Epoch: {i} Loss: {loss.item()}')
+
+        with torch.no_grad():  # turn off backpropogation
+            y_eval = self.model.forward(self.X_test)  # X_test are features from our test set, y_eval will be predictions
+            loss = self.criterion(y_eval, self.y_test)
+
+        correct = 0
+        wrong = 0
+        with torch.no_grad():
+            for i, data in enumerate(self.X_test):
+                y_val = self.model.forward(data)
+                if y_val.argmax().item() == self.y_test[i]:
+                    correct += 1
+                else:
+                    wrong += 1
+
+        print(f'We got {correct} correct and {wrong} wrong!, with an accuracy of {(correct / (correct + wrong)) * 100}')
+        return f'We got {correct} correct and {wrong} wrong!, with an accuracy of {(correct / (correct + wrong)) * 100}'
